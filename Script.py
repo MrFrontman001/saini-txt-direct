87b7e9adaf2c3b4dfebd843f011b89056f1db185
Mr Frontman <mohittatu2007@gmail.com>
2025-07-28 16:57:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,3 +12,61 @@ OWNER_COMMANDS_CAPTION = """
  ➠ <b>𝐌𝐚𝐝𝐞 𝐁𝐲 :</b> {credit} 💻  
 ╰────────⊰◆⊱────────╯
 """
+
+USER_COMMANDS_CAPTION = """
+💥 𝐁𝐎𝐓𝐒 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+📌 𝗠𝗮𝗶𝗻 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀:
+
+➥ /start – Bot Status Check  
+➥ /drm – Extract from .txt (Auto)  
+➥ /y2t – YouTube → .txt Converter  
+➥ /ytm – YouTube → .mp3 downloader  
+➥ /t2t – Text → .txt Generator  
+➥ /stop – Cancel Running Task  
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰  
+⚙️ 𝗧𝗼𝗼𝗹𝘀 & 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀:
+
+➥ /cookies – Update YT Cookies  
+➥ /id – Get Chat/User ID  
+➥ /info – User Details  
+➥ /logs – View Bot Activity  
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+💡 𝗡𝗼𝘁𝗲:
+
+• Send any link for auto-extraction  
+• Supports batch processing  
+
+╭────────⊰◆⊱────────╮  
+ ➠ 𝐌𝐚𝐝𝐞 𝐁𝐲 : {CREDIT} 💻  
+╰────────⊰◆⊱────────╯
+"""
+
+
+UPGRADE_COMMANDS_CAPTION = """
+🎉 Welcome [{first_name}](tg://user?id={user_id}) to DRM Bot! 🎉
+
+You can have access to download all Non-DRM+AES Encrypted URLs 🔐 including
+
+<blockquote>
+• 📚 Appx Zip+Encrypted Url  
+• 🎓 Classplus DRM+ NDRM  
+• 🧑‍🏫 PhysicsWallah DRM  
+• 📚 CareerWill + PDF  
+• 🎓 Khan GS  
+• 🎓 Study Iq DRM  
+• 🚀 APPX + APPX Enc PDF  
+• 🎓 Vimeo Protection  
+• 🎓 Brightcove Protection  
+• 🎓 Visionias Protection  
+• 🎓 Zoom Video  
+• 🎓 Utkarsh Protection(Video + PDF)  
+• 🎓 All Non DRM+AES Encrypted URLs  
+• 🎓 MPD URLs if the key is known (e.g., Mpd_url?key=key XX:XX)
+</blockquote>
+
+<b>💵 Monthly Plan: 100 INR</b>
+
+If you want to buy membership of the bot, feel free to contact [{CREDIT}](tg://user?id={OWNER})
+"""
