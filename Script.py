46bb03f7e8a80f22074aa2cbf76d9fe7050d44d5
Mr Frontman <mohittatu2007@gmail.com>
2025-07-28 15:33:46 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,16 @@
+# Script.py
+
+    OWNER_COMMANDS_CAPTION = """
+👤 <b>𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬</b>
+
+➥ /addauth xxxx – Add User ID  
+➥ /rmauth xxxx – Remove User ID  
+➥ /users – Total User List  
+➥ /broadcast – For Broadcasting  
+➥ /broadusers – All Broadcasting Users  
+➥ /reset – Reset Bot  
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰  
+╭────────⊰◆⊱────────╮  
+ ➠ <b>𝐌𝐚𝐝𝐞 𝐁𝐲 :</b> {credit} 💻  
+╰────────⊰◆⊱────────╯
+"""
