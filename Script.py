ce5b2b24d2843d9eb867cb3220008cc20a59108e
Mr Frontman <mohittatu2007@gmail.com>
2025-07-28 17:18:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,72 +1,62 @@
 OWNER_COMMANDS_CAPTION = """
-👤 <b>𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬</b>
+<b>👤 𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬
 
-➥ /addauth xxxx – Add User ID  
-➥ /rmauth xxxx – Remove User ID  
-➥ /users – Total User List  
-➥ /broadcast – For Broadcasting  
-➥ /broadusers – All Broadcasting Users  
-➥ /reset – Reset Bot  
+➥ /addauth xxxx – Aᴅᴅ Uꜱᴇʀ ID  
+➥ /ʀrmauth xxxx – Rᴇᴍᴏᴠᴇ Uꜱᴇʀ ID  
+➥ /users – Tᴏᴛᴀʟ Uꜱᴇʀ Lɪꜱᴛ  
+➥ /broadcast – Fᴏʀ Bʀᴏᴀᴅᴄᴀꜱᴛɪɴɢ  
+➥ /broadusers – Aʟʟ Bʀᴏᴀᴅᴄᴀꜱᴛɪɴɢ Uꜱᴇʀꜱ  
+➥ /reset – Rᴇꜱᴇᴛ Bᴏᴛ  
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰  
 ╭────────⊰◆⊱────────╮  
- ➠ <b>𝐌𝐚𝐝𝐞 𝐁𝐲 :</b> {credit} 💻  
-╰────────⊰◆⊱────────╯
+ ➠ 𝐎𝐰𝐧𝐞𝐫 𝐎𝐧𝐥𝐲  
+╰────────⊰◆⊱────────╯</b>
 """
 
 USER_COMMANDS_CAPTION = """
-💥 𝐁𝐎𝐓𝐒 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒
+<b>💥 𝐁𝐎𝐓𝐒 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 📌 𝗠𝗮𝗶𝗻 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀:
-
-➥ /start – Bot Status Check  
-➥ /drm – Extract from .txt (Auto)  
-➥ /y2t – YouTube → .txt Converter  
-➥ /ytm – YouTube → .mp3 downloader  
-➥ /t2t – Text → .txt Generator  
-➥ /stop – Cancel Running Task  
+<blockquote>➥ /drm – Exᴛʀᴀᴄᴛ ғʀᴏᴍ .ᴛxᴛ (Aᴜᴛᴏ)  
+➥ /y2t – YᴏᴜTᴜʙᴇ → .ᴛxᴛ Cᴏɴᴠᴇʀᴛᴇʀ  
+➥ /ytm – YᴏᴜTᴜʙᴇ → .ᴍᴘ𝟹 ᴅᴏᴡɴʟᴏᴀᴅᴇʀ  
+➥ /t2t– Tᴇxᴛ → .ᴛxᴛ Gᴇɴᴇʀᴀᴛᴏʀ  
+➥ /stop– Cᴀɴᴄᴇʟ Rᴜɴɴɪɴɢ Tᴀꜱᴋ  </blockquote>
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰  
 ⚙️ 𝗧𝗼𝗼𝗹𝘀 & 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀:
-
-➥ /cookies – Update YT Cookies  
-➥ /id – Get Chat/User ID  
-➥ /info – User Details  
-➥ /logs – View Bot Activity  
+<blockquote>➥ /cookies– Uᴘᴅᴀᴛᴇ YT Cᴏᴏᴋɪᴇꜱ  
+➥ /id – Gᴇᴛ Cʜᴀᴛ/Uꜱᴇʀ ID  
+➥ /info – Uꜱᴇʀ Dᴇᴛᴀɪʟꜱ  
+➥ /logs – Vɪᴇᴡ Bᴏᴛ Aᴄᴛɪᴠɪᴛʏ  </blockquote>
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 
 💡 𝗡𝗼𝘁𝗲:
 
-• Send any link for auto-extraction  
-• Supports batch processing  
-
-╭────────⊰◆⊱────────╮  
- ➠ 𝐌𝐚𝐝𝐞 𝐁𝐲 : {CREDIT} 💻  
-╰────────⊰◆⊱────────╯
-"""
-
+• Sᴇɴᴅ ᴀɴʏ ʟɪɴᴋ ғᴏʀ ᴀᴜᴛᴏ-ᴇxᴛʀᴀᴄᴛɪᴏɴ  
+• Sᴜᴘᴘᴏʀᴛꜱ ʙᴀᴛᴄʜ ᴘʀᴏᴄᴇꜱꜱɪɴɢ</b>"""
 
 UPGRADE_COMMANDS_CAPTION = """
-🎉 Welcome [{first_name}](tg://user?id={user_id}) to DRM Bot! 🎉
+<b>☘️ Wᴇʟᴄᴏᴍᴇ {first_name} ᴛᴏ DRM Bᴏᴛ! 🎉
 
-You can have access to download all Non-DRM+AES Encrypted URLs 🔐 including
+Yᴏᴜ ᴄᴀɴ ʜᴀᴠᴇ ᴀᴄᴄᴇꜱꜱ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴀʟʟ Nᴏɴ-DRM+AES Eɴᴄʀʏᴘᴛᴇᴅ URLꜱ 🔐 ɪɴᴄʟᴜᴅɪɴɢ
 
 <blockquote>
-• 📚 Appx Zip+Encrypted Url  
-• 🎓 Classplus DRM+ NDRM  
-• 🧑‍🏫 PhysicsWallah DRM  
-• 📚 CareerWill + PDF  
-• 🎓 Khan GS  
-• 🎓 Study Iq DRM  
-• 🚀 APPX + APPX Enc PDF  
-• 🎓 Vimeo Protection  
-• 🎓 Brightcove Protection  
-• 🎓 Visionias Protection  
-• 🎓 Zoom Video  
-• 🎓 Utkarsh Protection(Video + PDF)  
-• 🎓 All Non DRM+AES Encrypted URLs  
-• 🎓 MPD URLs if the key is known (e.g., Mpd_url?key=key XX:XX)
-</blockquote>
+• 📚 Aᴘᴘx Zɪᴘ+Eɴᴄʀʏᴘᴛᴇᴅ Uʀʟ  
+• 🎓 Cʟᴀꜱꜱᴘʟᴜꜱ DRM+ NDRM  
+• 🧑‍🏫 PʜʏꜱɪᴄꜱWᴀʟʟᴀʜ DRM  
+• 📚 CᴀʀᴇᴇʀWɪʟʟ + PDF  
+• 🎓 Kʜᴀɴ GS  
+• 🎓 Sᴛᴜᴅʏ Iǫ DRM  
+• 🚀 APPX + APPX Eɴᴄ PDF  
+• 🎓 Vɪᴍᴇᴏ Pʀᴏᴛᴇᴄᴛɪᴏɴ  
+• 🎓 Bʀɪɢʜᴛᴄᴏᴠᴇ Pʀᴏᴛᴇᴄᴛɪᴏɴ  
+• 🎓 Vɪꜱɪᴏɴɪᴀꜱ Pʀᴏᴛᴇᴄᴛɪᴏɴ  
+• 🎓 Zᴏᴏᴍ Vɪᴅᴇᴏ  
+• 🎓 Uᴛᴋᴀʀꜱʜ Pʀᴏᴛᴇᴄᴛɪᴏɴ(Vɪᴅᴇᴏ + PDF)  
+• 🎓 Aʟʟ Nᴏɴ DRM+AES Eɴᴄʀʏᴘᴛᴇᴅ URLꜱ  
+• 🎓 MPD URLꜱ ɪғ ᴛʜᴇ ᴋᴇʏ ɪꜱ ᴋɴᴏᴡɴ (ᴇ.ɢ., Mᴘᴅ_ᴜʀʟ?ᴋᴇʏ=ᴋᴇʏ XX:XX)</blockquote>
 
-<b>💵 Monthly Plan: 100 INR</b>
+💵 Mᴏɴᴛʜʟʏ Pʟᴀɴ: 𝟷𝟶𝟶 INR
 
-If you want to buy membership of the bot, feel free to contact [{CREDIT}](tg://user?id={OWNER})
-"""
+Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʙᴜʏ ᴍᴇᴍʙᴇʀꜱʜɪᴘ ᴏғ ᴛʜᴇ ʙᴏᴛ, ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ ᴄᴏɴᴛᴀᴄᴛ</b>"""
+# ध्यान दें: अब {CREDIT} या {credit} कहीं नहीं है।
